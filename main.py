00620fd71c44dfed90d5b51f9c1260d71467f755
Habounou <barh.hugo@gmail.com>
2024-11-10 18:22:15 -0500

    Base program

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+random_nbr = random.randint(0,100)
+
+while True:
+    user_text = ["***********","***********","***********"]
+    while len(user_text[0 and 1 and 2]) > 10:
+        user_text[0] = input("Enter 5 lines of characters to create a new card (maximum 10 characters per line):\n").lower()
+        for lines in range (0, 2):
+            user_text[lines + 1] = input("").lower()
+        if len(user_text[0 and 1 and 2]) > 10:
+            print("You have exceeded the character limit in a line.")
+
+    print("*----------------*")
+    print("|                |")
+    for new_lines in range (0, 3):
+        if len(user_text[new_lines]) % 2 == 0:
+            print("|   " + " " * ((10 - len(user_text[new_lines])) // 2) + user_text[new_lines] +
+                  " " * ((10 - len(user_text[new_lines])) // 2) + "   |")
+        else:
+            print("|   \u202F\u200A" + " " * ((10 - len(user_text[new_lines])) // 2) + user_text[new_lines] +
+                  " " * ((10 - len(user_text[new_lines])) // 2) + "\u202F   |")
+    print("|                |")
+    print("*----------------*")
+
+    redo = input("Are you satisfied with this rendering? (yes:Y/no:N)\n")
+    if redo.upper() == "Y":
+        break
+
+ascii_sum = 0
+for nbr_lines in range (0, 3):
+    ascii_sum = sum(ord(char) for char in user_text[nbr_lines])
+random.seed(ascii_sum)
