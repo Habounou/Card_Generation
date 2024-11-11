2dcc2652fb795cc8127a0cdabc68dc310b1f0564
Habounou <barh.hugo@gmail.com>
2024-11-11 13:34:23 -0500

    Base program version 3

--- main.py
+++ main.py
@@ -3,30 +3,31 @@ import random
 random_nbr = random.randint(0,100)
 
 def art_print (text, align = "c"):
-    print("*------------*")
+    print("┌────────────┐")
     if align == "l":
-        for new_lines in range(0, 3):
-            print("| " + text[new_lines] + " " * (10 - len(text[new_lines])) + " |")
+        for new_lines in range(0, 5):
+            print("│ " + text[new_lines] + " " * (10 - len(text[new_lines])) + " │")
     elif align == "r":
-        for new_lines in range(0, 3):
-            print("| " + " " * (10 - len(text[new_lines])) + text[new_lines] + " |")
+        for new_lines in range(0, 5):
+            print("│ " + " " * (10 - len(text[new_lines])) + text[new_lines] + " │")
     else:
-        for new_lines in range (0, 3):
+        for new_lines in range (0, 5):
             if len(text[new_lines]) % 2 == 0:
-                print("| " + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
-                      " " * ((10 - len(text[new_lines])) // 2) + " |")
+                print("│ " + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
+                      " " * ((10 - len(text[new_lines])) // 2) + " │")
             else:
-                print("| \u202F\u200A" + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
-                      " " * ((10 - len(text[new_lines])) // 2) + "\u202F |")
-    print("*------------*")
+                print("│ \u202F\u200A" + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
+                      " " * ((10 - len(text[new_lines])) // 2) + "\u202F │")
+    print("└────────────┘")
 
 while True:
-    user_text = ["***********","***********","***********"]
-    while len(user_text[0 and 1 and 2]) > 10:
-        user_text[0] = input("Enter 5 lines of characters to create a new card (maximum 10 characters per line):\n")
-        for lines in range (0, 2):
+    user_text = ["***********","***********","***********","***********","***********"]
+    while len(user_text[0 and 1 and 2 and 3 and 4]) > 10:
+        user_text[0] = input("Enter 5 lines of characters to create a new card (maximum 10 characters per line):\n"
+                             " ↓ ↓ ↓ ↓ ↓\n")
+        for lines in range (0, 4):
             user_text[lines + 1] = input("").lower()
-        if len(user_text[0 and 1 and 2]) > 10:
+        if len(user_text[0 and 1 and 2 and 3 and 4]) > 10:
             print("You have exceeded the character limit in a line.")
 
     art_print(user_text)
@@ -39,11 +40,25 @@ while True:
         else:
             break
 
+    print("┌──────────────────────────────────────────────────┐"
+          "│ ████████████████████████████████████████████████ │"
+          "│ █                                              █ │"
+          "│ █               ░▒▓▓▓▓▓▓▓▓▓▓▓▓▒░               █ │"
+          "│ █            ── DIGITAL DUEL CARD ──           █ │"
+          "│ █               ░▒▓▓▓▓▓▓▓▓▓▓▓▓▒░               █ │"
+          "│ █                                              █ │"
+          "│ ████████████████████████████████████████████████ │"
+          "├──────────────────────────────────────────────────┤"
+          "│        Card Name: ███████████████████████        │"
+          "│        Attribute: ████████                       │"
+          "│ ──────────────────────────────────────────────── │"
+          "│                                                  │")
+
     redo = input("Are you satisfied with this rendering? (yes:Y/no:N)\n")
     if redo.upper() == "Y":
         break
 
 ascii_sum = 0
-for nbr_lines in range (0, 3):
+for nbr_lines in range (0, 5):
     ascii_sum = sum(ord(char) for char in user_text[nbr_lines])
 random.seed(ascii_sum)
