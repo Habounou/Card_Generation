5d0ea467d477729a22df5cd73ee533ac098cf67a
Habounou <barh.hugo@gmail.com>
2024-11-11 13:39:37 -0500

    Base program version 3

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import random
 random_nbr = random.randint(0,100)
 
 def art_print (text, align = "c"):
-    print("┌────────────┐")
+    print("┌────────────────────────┐")
     if align == "l":
         for new_lines in range(0, 5):
-            print("│ " + text[new_lines] + " " * (10 - len(text[new_lines])) + " │")
+            print("│  " + text[new_lines] + " " * (20 - len(text[new_lines])) + "  │")
     elif align == "r":
         for new_lines in range(0, 5):
-            print("│ " + " " * (10 - len(text[new_lines])) + text[new_lines] + " │")
+            print("│  " + " " * (20 - len(text[new_lines])) + text[new_lines] + "  │")
     else:
         for new_lines in range (0, 5):
             if len(text[new_lines]) % 2 == 0:
-                print("│ " + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
-                      " " * ((10 - len(text[new_lines])) // 2) + " │")
+                print("│  " + " " * ((20 - len(text[new_lines])) // 2) + text[new_lines] +
+                      " " * ((20 - len(text[new_lines])) // 2) + "  │")
             else:
-                print("│ \u202F\u200A" + " " * ((10 - len(text[new_lines])) // 2) + text[new_lines] +
-                      " " * ((10 - len(text[new_lines])) // 2) + "\u202F │")
-    print("└────────────┘")
+                print("│  \u202F\u200A" + " " * ((20 - len(text[new_lines])) // 2) + text[new_lines] +
+                      " " * ((20 - len(text[new_lines])) // 2) + "\u202F  │")
+    print("└────────────────────────┘")
 
 while True:
     user_text = ["***********","***********","***********","***********","***********"]
-    while len(user_text[0 and 1 and 2 and 3 and 4]) > 10:
-        user_text[0] = input("Enter 5 lines of characters to create a new card (maximum 10 characters per line):\n"
-                             " ↓ ↓ ↓ ↓ ↓\n")
+    while len(user_text[0 and 1 and 2 and 3 and 4]) > 20:
+        user_text[0] = input("Enter 5 lines of characters to create a new card (maximum 20 characters per line):\n"
+                             " ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓\n")
         for lines in range (0, 4):
             user_text[lines + 1] = input("").lower()
-        if len(user_text[0 and 1 and 2 and 3 and 4]) > 10:
+        if len(user_text[0 and 1 and 2 and 3 and 4]) > 20:
             print("You have exceeded the character limit in a line.")
 
     art_print(user_text)
