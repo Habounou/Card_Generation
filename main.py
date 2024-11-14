aa719dd609cd579e2de5f73e313c37011f2d7ad1
Habounou <barh.hugo@gmail.com>
2024-11-14 14:18:41 -0500

    Full program version 1

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def row_completion_c(text, length = 48):
 def card_search(card_list_ref, s_criteria):
     s_results = []
     for cards in range(0, (len(card_list_ref) - 1) // 9):
-        for elements in range(0, 10):
+        for elements in range(0, 9):
             if elements != 5:
                 if s_criteria in card_list_ref[9 * cards + elements]:
                     s_results += card_list_ref[9 * cards: 9 * cards + 9]
+                    break
     if s_results:
         s_results += [""]
     return s_results
